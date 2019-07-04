920371078046541d141750cfe3fbb519688f88f1
Marcel de Boer <marceldeboer@web.de>
2019-07-04 22:25:19 +0200

    Initial Commit - occupancy networks code - dataloader for ct images

---
+++ im2mesh/data/ct_dataloading.py
@@ -0,0 +1,87 @@
+import os
+
+import numpy as np
+from medpy.io import load
+from torch.utils.data import Dataset, DataLoader
+import torchvision
+from im2mesh.data import ct_transforms
+
+LABEL_SUFFIX = "_label_"  # followed by a number and the file format
+MHA_FORMAT = ".mha"
+
+
+class CTImagesDataset(Dataset):
+    def __init__(self, root_dir):
+        """
+        Args:
+            root_dir (string): Directory with the subdirectories for each image.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.root_dir = root_dir
+        self.sub_dirs = os.listdir(root_dir)
+        # store the path for each image and it's labels in a list [ [imagepath, [labelpath1, labelpath2, ...]] ]
+        allfiles = []
+        for sub_dir in self.sub_dirs:
+            sub_dir_files = os.listdir(os.path.join(self.root_dir, sub_dir))
+            for filename in sub_dir_files:
+                if filename.endswith(MHA_FORMAT) and LABEL_SUFFIX not in filename:
+                    image_filepath = os.path.join(self.root_dir, sub_dir, filename)
+                    label_filepaths = [os.path.join(self.root_dir, sub_dir, fn) for fn in
+                                   sub_dir_files if
+                                   LABEL_SUFFIX in fn and fn.endswith(MHA_FORMAT) and filename[0: -4] in fn]
+
+                    allfiles.append([image_filepath, label_filepaths])
+        self.allfiles = allfiles
+
+    def __len__(self):
+        return len(self.allfiles)
+
+    def __getitem__(self, idx):
+        image = load(self.allfiles[idx][0])[0]  # only take the image data, not the header
+        image_shape = image.shape
+
+        mha_labels = []
+        for labelpath in self.allfiles[idx][1]:
+            mha_labels.append(load(os.path.join(labelpath)))
+        label = load_label_mask(mha_labels, image_shape)
+
+        transformations = [ct_transforms.ReplicateBorderPadding3D((image_shape[0], 448, image_shape[2])),
+                           ct_transforms.ToTensor(),
+                           ct_transforms.Rescale((32, 32, 32))]
+        # if z dim is too small, pad to 512
+        if image_shape[2] <= 512:
+            transformations.insert(0, ct_transforms.ReplicateBorderPadding3D((image_shape[0], image_shape[1], 512)))
+        # otherwise crop center 512 of z dim
+        else:
+            transformations.insert(0, ct_transforms.CropZCenter3D(512))
+
+        # compose all transformations to one
+        composed_transform = torchvision.transforms.Compose(transformations)
+        image = composed_transform(image)
+        label = composed_transform(label)
+
+        sample = {'image': image, 'label': label}
+        return sample
+
+
+def load_label_mask(mha_label_list, mask_size):
+    merged_masks = np.zeros(mask_size, dtype=np.int)
+    for mha_label in mha_label_list:
+        label_box = mha_label[0]
+        label_offsets = mha_label[1].offset
+        label_mask = np.zeros(mask_size, dtype=np.int)
+        # expand the labels to the full size (same size as the image)
+        label_mask[int(label_offsets[0]):int(label_offsets[0] + label_box.shape[0]),
+        int(-label_offsets[1]):int(-label_offsets[1] + label_box.shape[1]),
+        int(label_offsets[2]):int(label_offsets[2] + label_box.shape[2])] = label_box
+
+        merged_masks = np.bitwise_or(merged_masks, label_mask)
+    return merged_masks
+
+
+if __name__ == '__main__':
+    data = CTImagesDataset("/home/marcel/Uni/Master/1.Semester/VC Praktikum/CTSamples")
+    for datax in data:
+        print("lala")
+    print("lala")
---
+++ im2mesh/data/ct_transforms.py
@@ -0,0 +1,104 @@
+import torch
+import torch.nn.functional as tf
+import numpy as np
+
+
+class Rescale(object):
+    """Rescale the tensor in a sample to a given size.
+
+    Args:
+        output_size (tuple or int): Desired output size.
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (tuple))
+        self.output_size = output_size
+
+    def __call__(self, image):
+        """
+        Expect 4 dim tensor in C x D x H x W format
+        """
+        assert isinstance(image, torch.Tensor)
+
+        image = tf.interpolate(image.view((1,) + image.size()).double(), size=self.output_size, mode='nearest')
+        image = image.view((1,) + self.output_size)
+        return image
+
+
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, image):
+        # swap depth axis because
+        # numpy image: H x W x D
+        # torch image: C x D x H x W
+        image = image.transpose((2, 0, 1))
+        image = image[np.newaxis,...]  # dim for channels
+        image = torch.from_numpy(image.astype('int32'))
+        return image
+
+
+class ReplicateBorderPadding3D(object):
+    """Pad the ndarray in a sample to a given size.
+
+    Args:
+        output_size (3-tuple): output size of the padded image
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (tuple))
+        self.output_size = output_size
+
+    def __call__(self, image):
+        """
+        expect 3 dim ndarray in H x W x D
+        """
+        assert isinstance(image, np.ndarray)
+
+        # pad image to the given size
+        x_pad = self.output_size[0] - image.shape[0]
+        y_pad = self.output_size[1] - image.shape[1]
+        z_pad = self.output_size[2] - image.shape[2]
+        if x_pad < 0 or y_pad < 0 or z_pad < 0:
+            raise ("Image dimension is bigger than the output size")
+
+        # determine 'before' and 'after' for each dimension
+        x_pad_bf = (x_pad // 2)
+        x_pad_af = x_pad - x_pad_bf
+        y_pad_bf = (y_pad // 2)
+        y_pad_af = y_pad - y_pad_bf
+        z_pad_bf = (z_pad // 2)
+        z_pad_af = z_pad - z_pad_bf
+
+        image = np.pad(image, ((x_pad_bf, x_pad_af), (y_pad_bf, y_pad_af), (z_pad_bf, z_pad_af)), 'edge')
+        return image
+
+
+class CropZCenter3D(object):
+    """Crop the center pixel of the Z dimension
+
+    Args:
+        center_size : center_size in pixels
+    """
+
+    def __init__(self, center_size):
+        assert isinstance(center_size, (int))
+        self.center_size = center_size
+
+    def __call__(self, image):
+        """
+        expect 3 dim ndarray in H x W x D
+        """
+        assert isinstance(image, np.ndarray)
+
+        # find out diff between actual size and center_size
+        z_diff = image.shape[2] - self.center_size
+        if z_diff < 0:
+            raise ValueError("Image dimension is smaller than the center size")
+
+        # crop 'center' X x Y x center_size pixels of the image.
+        image = image[:, :, z_diff:z_diff + self.center_size]
+
+        return image
+
+
