099cfcf27e3e9dbdfb95f0877c378b2b96c32989
Sebastian Ochs <sebastian.ochs@stud.tu-darmstadt.de>
2019-07-11 18:29:47 +0200

    Update ct_dataloading.py

--- im2mesh/data/ct_dataloading.py
+++ im2mesh/data/ct_dataloading.py
@@ -27,22 +27,23 @@ class CTImagesDataset(Dataset):
         self.sub_dirs = [x for x in os.listdir(root_dir) if os.path.isdir(os.path.join(root_dir, x))]
         # store the path for each image and it's labels in a list [ [imagepath, [labelpath1, labelpath2, ...]] ]
         allfiles = []
-        while len(allfiles) < 10:
-            print("datalength: ", len(allfiles))
-            for sub_dir in self.sub_dirs :
-                sub_dir_files = os.listdir(os.path.join(self.root_dir, sub_dir))
-                for filename in sub_dir_files:
-                    if filename.endswith(MHA_FORMAT) and LABEL_SUFFIX not in filename:
-                        # Image paths
-                        image_filepath = os.path.join(self.root_dir, sub_dir, filename)
-                        # Label paths
-                        label_filepaths = [os.path.join(self.root_dir, sub_dir, labelname)
-                                            for labelname in sub_dir_files
-                                                if LABEL_SUFFIX in labelname and labelname.endswith(MHA_FORMAT)
-                                                    and filename[0:-4] in labelname]
-                        # Append paths from found images with corresponding labels
-                        allfiles.append([image_filepath, label_filepaths])
-
+        print("datalength: ", len(allfiles))
+        for sub_dir in self.sub_dirs:
+            sub_dir_files = os.listdir(os.path.join(self.root_dir, sub_dir))
+            for filename in sub_dir_files:
+                if filename.endswith(MHA_FORMAT) and LABEL_SUFFIX not in filename:
+                    # Image paths
+                    image_filepath = os.path.join(self.root_dir, sub_dir, filename)
+                    # Label paths
+                    label_filepaths = [os.path.join(self.root_dir, sub_dir, labelname)
+                                        for labelname in sub_dir_files
+                                            if LABEL_SUFFIX in labelname and labelname.endswith(MHA_FORMAT)
+                                                and filename[0:-4] in labelname]
+                    # Append paths from found images with corresponding labels
+                    allfiles.append([image_filepath, label_filepaths])
+                    if(len(allfiles) > 9):
+                        break
+                    print("datalength: ", len(allfiles))
         self.allfiles = allfiles
 
 
