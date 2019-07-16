28b20fa8ca0f49be3bf9b10803f89d14006e0397
Sebastian Ochs <sebastian.ochs@stud.tu-darmstadt.de>
2019-07-16 13:19:41 +0200

    Update ct_dataloading.py

--- im2mesh/data/ct_dataloading.py
+++ im2mesh/data/ct_dataloading.py
@@ -71,10 +71,10 @@ class CTImagesDataset(Dataset):
         labels = [label_transform(label) for label in labels]
 
         points, points_occ = self._sample_points_inside_boundingboxes(labels, 1024)
-        print(type(points[0][0]))
-        print(type(points_occ[0]))
+        
 
         sample = {'points': points.astype(float), 'points.occ': points_occ.astype(float), 'inputs': image}
+        print(type(points.astype(float)[0][0]))
         return sample
 
 
