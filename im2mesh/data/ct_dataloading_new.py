80ff113db5fde72ac933e89fe68c021c8cbbbb1f
Sebastian Ochs <sebastian.ochs@stud.tu-darmstadt.de>
2019-08-07 12:00:13 +0200

    Testing new dataloading

--- im2mesh/data/ct_dataloading_new.py
+++ im2mesh/data/ct_dataloading_new.py
@@ -66,12 +66,9 @@ class CTImagesDataset(Dataset):
             transformations.insert(0, ct_transforms.CropZCenter3D(512))
         # compose all transformations to one
         image_transform = torchvision.transforms.Compose(transformations)
-        # Change here
-        # label_transform = torchvision.transforms.Compose(transformations[0:-1])  # do not downscale the labels
-
         image = image_transform(image)
-        labels = determine_bounding_boxes(image_shape, mha_labels)
-
+        #
+        labels = determine_offsets(image_shape, mha_labels, 512)
         points, points_occ = self._sample_points_inside_boundingboxes(labels, 1024)
 
 
@@ -79,53 +76,72 @@ class CTImagesDataset(Dataset):
         return sample
 
     # Determine bounding boxes
-    def determine_bounding_boxes(image_shape, label_list):
-        # Copy labels
-        labels = label_list.copy()
-        # Change offsets and label boxes, if necessary
+    def determine_offsets(shape, label_list, z):
+        """
+        Determine the offset for each label so we can use these for indexing.
+        The voxel spacing will not be taken into account for the new offset, but can be applied again via the label!
+        Remove labels from list, if they are partially or completely out of the new cropped image
+        :param image_shape: the shape of the image (620, 420, z), determines if image is being cropped or padded
+        :param label_list: A list of labels for the corresponding image
+        :param z: Number that indicates
+        :return: list of tuples with tuples being (new offset, label), number of discarded labels
+        """
+        offsets_and_labels = []
+        discarded = 0
+        # x and y are fixed to 620, 420. Remember y, because offset y is inverted
+        y_max = 420
 
-        if shape[2]> 512:
-            # Image will be cropped from center
-            image_z_center = shape[2] // 2
-            # Z-offset changes by the following amount
-            image_z_offset = (shape[2] - 512) // 2
-            
+        # Padding changes from (620, 420, z) to (640, 448, 512):
+        x_pad = 10
+        y_pad = 14
+
+        # If z-dim exceeds z, image will be cropped
+        if shape[2]> z:
+            # Calculate the borders of the z_dim
+            z_diff = (shape[2] - z) // 2
+            begin = z_diff
+            end = z_diff + z
+
             # List for labels that will be removed
             remove = []
             for label in labels:
-                # Divide by voxelspacing to get index
+
+                # Voxelspacing for z_dim
                 voxel_spacing = label[1].voxel_spacing[2]
+                # Offset changes:
+                y_offset = y_max - label[1].offset[1]
                 z_offset = int(round(label[1].offset[2] / voxel_spacing))
-                z_label_size = label[0].shape[2]
-                z_begin = image_z_center - 256
-                z_end = image_z_center + 256
-                # First case: label is completely inside the cropped image
-                if (z_offset > z_begin and (z_offset + z_label_size) < z_end):
-                    # Offset needs to change:
-                    label[1].offset[2] = (z_offset + image_z_offset) * voxel_spacing
-                # Remove all other labels
-                else:                    
-                    remove.append(label)
-                
-            # Remove labels from list
-            if len(remove) > 0:
-                labels = [x for x in labels if x not in remove]
+
+                #If label is completely inside the cropped image
+                if z_offset > begin and (z_offset + label[0].shape[2]) < z_end):
+                    # Offset change:
+                    offset = [label[1].offset[0] + x_pad, y_offset + y_pad, z_offset + z_diff]
+                    offsets_and_labels.append((offset, label))
+                # Else: label will be not be appended, counter for discarded labels increases
+                else:
+                    discarded = discarded + 1
+        # z will be padded from both sides
         else:
-            # Image will be padded from both sides
-            change = (512 - shape[2]) // 2
+            # Calculate padding for z
+            z_pad = (z - label[0].shape[2]) // 2
             # Add change to offsets
             for label in labels:
+                # Voxelspacing for z_dim
                 voxel_spacing = label[1].voxel_spacing[2]
-                label[1].offset[2] = ((label[1].offset[2] / voxel_spacing) + change) * voxel_spacing
+                # Offset changes:
+                y_offset = y_max - label[1].offset[1]
+                z_offset = int(round(label[1].offset[2] / voxel_spacing))
+                offset = [label[1].offset[0] + x_pad, y_offset + y_pad, z_offset + z_pad]
+                offsets_and_labels.append((offset, label))
 
-        return labels 
-   
+        return offsets_and_labels, discarded
+
 
     # label_masks
     def _sample_points_inside_boundingboxes(self, labels, num_points):
         """
         Sample a given number of points from the bounding box of an object.
-        :param labels: list of labels with new offset for the objects inside the ct image
+        :param labels: list of tuples (new offset, label) for labels inside the ct image
         :param num_points: the number of points to draw in total
         :return: a list of x,y,z coordinate pairs with length num_points
         """
@@ -145,52 +161,51 @@ class CTImagesDataset(Dataset):
             :param limit_tuple: 6 - tuple of limits for all dimensions (x_min, x_max, y_min, y_max, z_min, z_max)
             :return:
             """
-            x_koords = np.round(np.random.uniform(low=limit_tuple[0], high=limit_tuple[1], size=(num_points, 1)), 3)
-            y_koords = np.round(np.random.uniform(low=limit_tuple[2], high=limit_tuple[3], size=(num_points, 1)), 3)
-            z_koords = np.round(np.random.uniform(low=limit_tuple[4], high=limit_tuple[5], size=(num_points, 1)), 3)
-            points_xyz = np.hstack((x_koords, y_koords, z_koords))
+            x_coords = np.round(np.random.uniform(low=limit_tuple[0], high=limit_tuple[1], size=(num_points, 1)), 3)
+            y_coords = np.round(np.random.uniform(low=limit_tuple[2], high=limit_tuple[3], size=(num_points, 1)), 3)
+            z_coords = np.round(np.random.uniform(low=limit_tuple[4], high=limit_tuple[5], size=(num_points, 1)), 3)
+            points_xyz = np.hstack((x_coords, y_coords, z_coords))
             return points_xyz
 
         def lookup_occ(label, points):
             """
             look up occupancy value at nearest neighbour for each given point
+            :param label: tuple of (new offset, label)
+            :param points: list of points to look up inside the label
             :return: occupancy values for points
             """
-            nearest_points = np.round(points).astype(int)
-            # we need
             # Label offset:
-            x_offset = label[1].offset[0] 
-            y_offset = label[1].offset[1] 
-            z_offset = int(round(label[1].offset[2] / voxel_spacing))  
-            occ_val = label[nearest_points[:,0], nearest_points[:, 1], nearest_points[:,2]]
-            return occ_val
+            offset = label[0]
+            # List of nearest points, subtract offset from points to lookup point in label:
+            nearest_points = [np.subtract(point, offset) for point in np.round(points).astype(int)]
+            # Look up occupancy values of points
+            return label[1][0][nearest_points[:,0], nearest_points[:, 1], nearest_points[:,2]]
+
         # change
-        def recover_boundingbox(label):
+        def bounding_box_limit(label):
             """
-            Get dimensions of bounding box from label
+            Get limits of bounding box of the label inside the image
+            :param label: tuple of (new offset, label)
             :return: 6 tuple with start and end of the bounding box in each dimension
             """
-            shape = label.shape
-            x_offset = label[1].offset[0] 
-            y_offset = label[1].offset[1] 
-            z_offset = int(round(label[1].offset[2] / voxel_spacing)) 
+            shape = label[1][0].shape
+            offset = label[0]
+            x_low = x_offset
+            x_high = x_offset + shape[0]
+            y_low = y_offset
+            y_high = y_offset + shape[1]
+            z_low = z_offset
+            z_high = z_offset+ shape[2]
 
-            x_idxs = [x for x in range(shape[0]) if np.sum(label[x, :, :]) != 0]
-            x_low, x_high = min(x_idxs), max(x_idxs) + 1
-            y_idxs = [y for y in range(shape[1]) if np.sum(label[:, y, :]) != 0]
-            y_low, y_high = min(y_idxs), max(y_idxs) + 1
-            z_idxs = [z for z in range(shape[2]) if np.sum(label[:, :, z]) != 0]
-            z_low, z_high = min(z_idxs), max(z_idxs) + 1
-
-            return x_low + x_offset, x_high + x_offset, y_low + y_offset, y_high + y_offset, z_low + z_offset, z_high + z_offset
+            return (x_low, x_high, y_low, y_high, z_low, z_high)
 
         # sample points from each bounding box
         all_points = np.array([np.inf, np.inf, np.inf]).reshape(1,3)  # remove dummy entry later
         all_occ = np.array([np.inf]).reshape(1,)  # remove dummy entry later
         # sample points and occ values for each bounding box/ label
         for i, label in enumerate(labels):
-            bounding_box_limits = recover_boundingbox(label)
-            points = sample_points(points_per_label[i], bounding_box_limits)
+            limit = bounding_box_limit(label)
+            points = sample_points(points_per_label[i], limit)
             occ = lookup_occ(label, points)
             all_points = np.vstack((all_points, points))
             all_occ = np.append(all_occ, occ)
